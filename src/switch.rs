cf22af9ca2eb2d66a04c99dc3341a5a31ca2f75c
Henry Zimmerman <zimhen7@gmail.com>
2019-10-10 12:12:30 -0400

    replace FromCapturedKeyValue with just Switch, allowing recursive Switching

--- src/switch.rs
+++ src/switch.rs
@@ -1,5 +1,7 @@
 //! Route based on enums.
 use crate::route_info::RouteInfo;
+use crate::RouteState;
+use std::str::FromStr;
 
 /// Routing trait for enums
 ///
@@ -29,7 +31,64 @@ use crate::route_info::RouteInfo;
 ///
 pub trait Switch: Sized {
     /// Based on a route, possibly produce an itself.
-    fn switch<T>(route: RouteInfo<T>) -> Option<Self>;
+    fn switch<T: RouteState>(route: RouteInfo<T>) -> Option<Self>;
+
+    /// If the key isn't available, this will be called.
+    /// This allows an implementation to provide a default when matching fails instead of outright failing the parse.
+    fn key_not_available() -> Option<Self> {
+        None
+    }
 }
 
+
+impl<U: Switch> Switch for Option<U> {
+    fn switch<T: RouteState>(route: RouteInfo<T>) -> Option<Self> {
+        Some(Some(Switch::switch(route)?))
+    }
+
+    /// This will cause the derivation of `from_matches` to not fail if the key can't be located
+    fn key_not_available() -> Option<Self> {
+        Some(None)
+    }
+}
+
+impl<U, E> Switch for Result<U, E>
+    where
+        U: FromStr<Err = E>,
+{
+    fn switch<T: RouteState>(route: RouteInfo<T>) -> Option<Self> {
+        Some(U::from_str(&route.route))
+    }
+}
+
+
+macro_rules! impl_switch_for_from_str {
+    ($($SelfT: ty),*) => {
+        $(
+        impl Switch for $SelfT {
+            fn switch<T>(route: RouteInfo<T>) -> Option<Self> {
+                std::str::FromStr::from_str(&route.route).ok()
+            }
+        }
+        )*
+    };
+}
+
+impl_switch_for_from_str! {
+    String,
+    usize,
+    u128,
+    u64,
+    u32,
+    u16,
+    u8,
+    isize,
+    i128,
+    i64,
+    i32,
+    i16,
+    i8,
+    f64,
+    f32
+}
 
